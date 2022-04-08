7b96419d573d112c5e1a8692751c23a85be93243
Dmiroshnikov <ssteamm90@gmail.com>
2022-04-08 23:26:03 +0300

    Initial Commit

---
+++ testGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by Дмитрий Мирошников on 08.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
